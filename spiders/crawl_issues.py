71aecb71639c2ba4d62135a19b6a107d3d383030
edsonw <969693577@qq.com>
2018-01-25 10:03:21 +0800

    annotate

--- spiders/crawl_issues.py
+++ spiders/crawl_issues.py
@@ -21,35 +21,6 @@ class CrawlIssues(scrapy.Spider):
     name = 'crawl_issues'
     allowed_domains = ['api.github.com']
     start_urls = ['https://api.github.com']
-    tokens = ["34d9931d22c5ba2c48bd93cc3e6e1b5b2bc7620d", "237b3763792474c3e3c94b6fb47fbed2f93bef14",
-              "098b89fffb0a9d51f9c6e32520438461d4381cb1", "b2200a1d9afed61322a1a5e0332ba63edece6f09",
-              "f71f3953a533f2638b2041c9420af3406e697b9d", "c05088d8fbf49fec5e52d01c5faf171e6af7a139",
-              '34f6ded60da1f41641bfdb80377fa26ede35ed1e',
-              'c72b8e1b9f65deb45fd4327cff09ededec4a463c',
-              '5797dbb74e6fe2a1f6e3e73b0d1c7ee2f7497e76',
-              '132e61200f189b7f5f32616e3ce7f15319f6c164',
-              '8a7a5ee845b1bc461aa724990af41512b9c2e03d',
-              '368538619ef48db84dafcb5109bb88216b5566f3',
-              "35fd72bd13a7dcdd0eb3eeca30da6d58d9305e12",
-              "29ec7f1e8d8e24e590f7e720e436f59ff41b904f",
-              "1de182a2c30734e335241559449b0e4422f5c503"
-              ]
-
-    token_list = [
-        '293a06ac6ed5a746f7314be5a25f3df2948fa501',
-        '66de084042a7d3311544c656ad9273927dc73408',
-        'a513f61368e16c2da229e38e139a8e5e25848a5c',
-        '9055150c8fd031468af71cbb4e12c550e8d2ec2b',
-        'ba119dc83af804327fa9dad8e07718729490c925',
-        'c9c13e5c14d6876c76919520c9b05d0c65faf18a',
-        '3e41cbfc0c8878aec935fba68a0d3c778f557a32',
-        '402ff55399ca08ca7c886a2031f49f16be6c2331',
-        '7cb6e20a24000968983b79b5de705c400f400588',
-        'd147a86290a1828205ec287d0c722ffbd66cd4f9',
-        '6b6956dbbc7a3249ce985e40226ed4ff793aa1df',
-        'de6ec0e027d89147a111d2be266bf35e45afd067'
-
-    ]
 
     it = itertools.cycle(tokens)
 
